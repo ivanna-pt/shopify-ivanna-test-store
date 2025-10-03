c43ba14d22c743f295baa86cc7ecbad19230d3eb
ivanna-pt <ivanna.pokhyl@gmail.com>
2025-10-03 14:09:47 +0300

    feat: re-render images, price and quantity using Section Rendering API

--- assets/banner-product.js
+++ assets/banner-product.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const currentSection = document.querySelector(".section__product-banner");
   const colorButtons = document.querySelectorAll(".color-button");
   const quantityDisplay = document.querySelector(
     ".product--inventory_quantity>span"
@@ -7,22 +8,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   colorButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      const variantId = button.getAttribute("data-variant-id");
-      const color = button.getAttribute("data-color");
-      const available = button.getAttribute("data-available");
+      const variantId = button.dataset.variantId;
+      const productHandle = button.dataset.productHandle;
 
       // Handle color selection
       colorButtons.forEach((btn) => btn.classList.remove("active"));
       button.classList.add("active");
 
+      // Update variant information with Section Rendering API
+      const url = `/products/${productHandle}?variant=${variantId}&sections=banner-product`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const tempDiv = document.createElement("div");
+          tempDiv.innerHTML = data["banner-product"];
+          currentSection.querySelector(".price-container").innerHTML =
+            tempDiv.querySelector(".price-container").innerHTML;
+          currentSection.querySelector(
+            ".product--inventory_quantity"
+          ).innerHTML = tempDiv.querySelector(
+            ".product--inventory_quantity"
+          ).innerHTML;
+          currentSection.querySelector(".product-images").innerHTML =
+            tempDiv.querySelector(".product-images").innerHTML;
+        });
+
       // Update quantity display
-      if (available > 0) {
-        quantityDisplay.textContent = `In stock: ${available}`;
-      } else if (available == "available") {
-        quantityDisplay.textContent = "In stock";
-      } else {
-        quantityDisplay.textContent = "Out of stock";
-      }
+      // if (available > 0) {
+      //   quantityDisplay.textContent = `In stock: ${available}`;
+      // } else if (available == "available") {
+      //   quantityDisplay.textContent = "In stock";
+      // } else {
+      //   quantityDisplay.textContent = "Out of stock";
+      // }
     });
   });
 
