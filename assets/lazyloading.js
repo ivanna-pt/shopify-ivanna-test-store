e2bae14325f02a2b87ff60b2b0ec43761b54037e
ivanna-pt <ivanna.pokhyl@gmail.com>
2025-11-25 12:27:30 +0200

    fix: disable lazy load os Swiper assets

--- assets/lazyloading.js
+++ assets/lazyloading.js
@@ -1,44 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let loaded = false;
+// document.addEventListener("DOMContentLoaded", () => {
+//   let loaded = false;
 
-  const scriptUrls = [
-    "/cdn/shop/t/13/assets/swiper-bundle.min.js?v=153095317606561337801763567068",
-  ];
+//   const scriptUrls = [
+//     "/cdn/shop/t/13/assets/swiper-bundle.min.js?v=153095317606561337801763567068",
+//   ];
 
-  const styleUrls = [
-    "/cdn/shop/t/13/assets/swiper-bundle.min.css?v=178409850749087333521763567068",
-  ];
+//   const styleUrls = [
+//     "/cdn/shop/t/13/assets/swiper-bundle.min.css?v=178409850749087333521763567068",
+//   ];
 
-  const loadAssets = () => {
-    if (loaded) return;
-    loaded = true;
+//   const loadAssets = () => {
+//     if (loaded) return;
+//     loaded = true;
 
-    scriptUrls.filter(Boolean).forEach((url) => {
-      const s = document.createElement("script");
-      s.src = url;
-      s.defer = true;
-      document.head.appendChild(s);
-    });
+//     scriptUrls.filter(Boolean).forEach((url) => {
+//       const s = document.createElement("script");
+//       s.src = url;
+//       s.defer = true;
+//       document.head.appendChild(s);
+//     });
 
-    styleUrls.filter(Boolean).forEach((url) => {
-      const l = document.createElement("link");
-      l.rel = "stylesheet";
-      l.href = url;
-      document.head.appendChild(l);
-    });
+//     styleUrls.filter(Boolean).forEach((url) => {
+//       const l = document.createElement("link");
+//       l.rel = "stylesheet";
+//       l.href = url;
+//       document.head.appendChild(l);
+//     });
 
-    console.log("Lazy-loaded Swiper assets");
+//     console.log("Lazy-loaded Swiper assets");
 
-    events.forEach((e) => document.removeEventListener(e, loadAssets));
-  };
+//     events.forEach((e) => document.removeEventListener(e, loadAssets));
+//   };
 
-  const events = ["click", "scroll", "touchstart", "mousemove", "keydown"];
-  events.forEach((e) => document.addEventListener(e, loadAssets));
+//   const events = ["click", "scroll", "touchstart", "mousemove", "keydown"];
+//   events.forEach((e) => document.addEventListener(e, loadAssets));
 
-  if ("requestIdleCallback" in window) {
-    requestIdleCallback(loadAssets, { timeout: 3000 });
-  }
-});
+//   if ("requestIdleCallback" in window) {
+//     requestIdleCallback(loadAssets, { timeout: 3000 });
+//   }
+// });
 
 (function () {
   if (!("IntersectionObserver" in window)) return;
